e16f542d3ec100d89038718116db41401b917cac
hudsonsr <44094473+hudsonsr@users.noreply.github.com>
2019-10-23 18:31:28 -0300

    Configurar sistema de filas

---
+++ src/lib/Queue.js
@@ -0,0 +1,47 @@
+import Bee from 'bee-queue';
+import CancellationMail from '../app/jobs/CancellationMail';
+import redisConfig from '../config/redis';
+
+const jobs = [CancellationMail];
+
+class Queue {
+  constructor() {
+    this.queues = {};
+
+    this.init();
+  }
+
+  /**
+   * Configuração da fila
+   */
+  init() {
+    jobs.forEach(({ key, handle }) => {
+      this.queues[key] = {
+        bee: new Bee(key, {
+          redis: redisConfig,
+        }),
+        handle,
+      };
+    });
+  }
+
+  /**
+   * adicionar itens a fila
+   */
+  add(queue, job) {
+    return this.queues[queue].bee.createJob(job).save();
+  }
+
+  /**
+   * Processa Fila
+   */
+  processQueue() {
+    jobs.forEach(job => {
+      const { bee, handle } = this.queues[job.key];
+
+      bee.process(handle);
+    });
+  }
+}
+
+export default new Queue();
