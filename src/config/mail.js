5161341bc385f11b77846b13d35ec31d8e2b3dba
hudsonsr <44094473+hudsonsr@users.noreply.github.com>
2019-10-23 13:28:31 -0300

    enviar email apenas com texto

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '289cab8b67d468',
+    pass: '6d63a7875bb13a',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
